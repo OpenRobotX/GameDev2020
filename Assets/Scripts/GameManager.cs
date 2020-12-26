991758b17b2c609465f12c209b96fe5142b45d46
OpenRobotX <42492737+OpenRobotX@users.noreply.github.com>
2020-12-26 20:02:26 +0100

    Update
    - Key & Door added
    - Tilemap edit
    - Background edit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool key1Collected = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Key1")
+        {
+            key1Collected = true;
+            GameObject.Destroy(collision.transform.gameObject);
+        }
+        if (collision.transform.name == "Door1" && key1Collected)
+        {
+            GameObject.Destroy(collision.transform.gameObject);
+        }
+    }
+}
