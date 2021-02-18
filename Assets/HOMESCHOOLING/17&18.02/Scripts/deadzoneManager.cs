eb11d8b9fa06f5664509f9f39a7396c4c1b9921d
OpenRobotX <42492737+OpenRobotX@users.noreply.github.com>
2021-02-18 14:15:48 +0100

    Homeschooling
    - Deadzone added

---
+++ Assets/HOMESCHOOLING/17&18.02/Scripts/deadzoneManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deadzoneManager : MonoBehaviour
+{
+    public GameObject ui;
+
+    private void Start()
+    {
+        ui.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Player")
+        {
+            Debug.Log("Player is dead");
+            collision.GetComponent<PlayerMovement>().enabled = false;
+            collision.GetComponent<Animator>().enabled = false;
+            collision.GetComponent<Rigidbody2D>().freezeRotation = false;
+            collision.GetComponent<Rigidbody2D>().AddTorque(5);
+            ui.SetActive(true);
+        }
+    }
+}
